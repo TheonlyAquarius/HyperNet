94422bbd7c33eb57b0bacf80fcd36e4c8841d85c
TheonlyAquarius <100468463+TheonlyAquarius@users.noreply.github.com>
2025-08-13 02:26:54 -0700

    Integrate timestep-aware generator

--- src/diffusion/generator.py
+++ src/diffusion/generator.py
@@ -1,10 +1,19 @@
+import torch
 import torch.nn as nn
 
 class model_ref(nn.Module):
     def __init__(self, n):
         super().__init__()
-        self.l = nn.Linear(n, n)
+        self.tm1 = nn.Linear(1, n)
+        self.lm1 = nn.Linear(n * 2, n)
 
-    def forward(self, x):
-        return self.l(x)
+    def forward(self, w, t):
+        e = self.tm1(t)
+        x = torch.cat([w, e], 1)
+        return self.lm1(x)
+
+# KEY
+# model_ref: next weight estimator
+# tm1: timestep embedding layer
+# lm1: weight mapping layer
 
