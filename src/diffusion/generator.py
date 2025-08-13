5048331f73dd113fd255bcd8b01855def57c613d
TheonlyAquarius <100468463+TheonlyAquarius@users.noreply.github.com>
2025-08-13 02:29:38 -0700

    feat: add configurable generator

--- src/diffusion/generator.py
+++ src/diffusion/generator.py
@@ -1,10 +1,25 @@
 import torch.nn as nn
+from src.diffusion_adv.diffusion_model import AdvancedWeightSpaceDiffusion
 
-class model_ref(nn.Module):
+
+class gen_lin(nn.Module):
     def __init__(self, n):
         super().__init__()
         self.l = nn.Linear(n, n)
 
     def forward(self, x):
         return self.l(x)
+
+
+def make_gen(n, kind="linear", **kwargs):
+    if kind == "linear":
+        return gen_lin(n)
+    if kind == "diffusion":
+        return AdvancedWeightSpaceDiffusion(target_model_flat_dim=n, **kwargs)
+    raise ValueError
+
+
+# KEY
+# gen_lin: linear generator
+# make_gen: generator factory
 
--- src/diffusion/train_generator.py
+++ src/diffusion/train_generator.py
@@ -1,22 +1,34 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from .generator import model_ref as gen
+from .generator import make_gen
 from src.utils.data import ckpt_set
 
+
 def train(cfg):
-    ds = ckpt_set(cfg['dataset'])
-    dl = DataLoader(ds, batch_size=cfg['hyperparameters']['batch_size'], shuffle=True)
-    n = ds[0].numel()
-    m = gen(n)
-    opt = torch.optim.Adam(m.parameters(), lr=cfg['hyperparameters']['learning_rate'])
+    ds = ckpt_set(cfg["dataset"])
+    dl = DataLoader(ds, batch_size=cfg["hyperparameters"]["batch_size"], shuffle=True)
+    s = ds[0]
+    n = s[0].numel() if isinstance(s, tuple) else s.numel()
+    g_cfg = cfg.get("generator", {})
+    m = make_gen(n, g_cfg.get("type", "linear"), **g_cfg.get("args", {}))
+    opt = torch.optim.Adam(m.parameters(), lr=cfg["hyperparameters"]["learning_rate"])
     loss = nn.MSELoss()
-    for _ in range(cfg['hyperparameters']['epochs']):
+    for _ in range(cfg["hyperparameters"]["epochs"]):
         for d in dl:
-            o = m(d)
-            l = loss(o, d)
+            if isinstance(d, tuple):
+                w, t = d
+                o = m(w, t)
+                l = loss(o, w)
+            else:
+                o = m(d)
+                l = loss(o, d)
             opt.zero_grad()
             l.backward()
             opt.step()
     return m
+
+
+# KEY
+# train: generator trainer
 
